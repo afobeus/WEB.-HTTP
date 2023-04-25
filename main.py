67e6d5f82f8eb622d3a502b7930127cc33496fa7
Artem <artem.dzichkanets@mail.ru>
2023-04-25 14:54:40 +0300

    task guess the city

--- main.py
+++ main.py
@@ -1,23 +1,28 @@
+import random
+from io import BytesIO
+
 import requests
-import sys
+from PIL import Image
 
 geocoder_api_key = "40d1649f-0493-4b70-98ba-98533de7710b"
-organisations_api_key = "dda3ddba-c9ea-4ead-9010-f43fbc15c6e3"
 
-address = " ".join(sys.argv[1:])
+cities = ["Санкт-Петербург", "Ванкувер", "Нью-Йорк", "Париж", "Мадрид", "Белград"]
+cities_cords = []
+for city in cities:
+    geocode_url = "https://geocode-maps.yandex.ru/1.x/"
+    geocode_params = {"geocode": city, "apikey": geocoder_api_key, "format": "json"}
+    response = requests.get(geocode_url, params=geocode_params)
+    response_json = response.json()
+    pos = response_json["response"]["GeoObjectCollection"]["featureMember"][0]["GeoObject"]["Point"]["pos"].split(" ")
+    cities_cords.append(pos)
 
-geocode_url = "https://geocode-maps.yandex.ru/1.x/"
-geocode_params = {"geocode": address, "apikey": geocoder_api_key, "format": "json"}
-response = requests.get(geocode_url, params=geocode_params)
-response_json = response.json()
-
-pos = response_json["response"]["GeoObjectCollection"]["featureMember"][0]["GeoObject"]["Point"]["pos"].split(" ")
-longitude, latitude = pos
-
-geocode_params = {"geocode": f"{longitude},{latitude}", "apikey": geocoder_api_key,
-                  "format": "json", "kind": "district", "results": 1}
-response = requests.get(geocode_url, params=geocode_params)
-response_json = response.json()
-geo_object = response_json["response"]["GeoObjectCollection"]["featureMember"][0]["GeoObject"]
-district = geo_object["metaDataProperty"]["GeocoderMetaData"]["Address"]["Components"][4]["name"]
-print("район -", district)
+while True:
+    cords_index = random.randint(0, len(cities_cords) - 1)
+    longitude, latitude = cities_cords[cords_index]
+    map_type = random.choice(["map", "sat"])
+    static_map_url = "https://static-maps.yandex.ru/1.x/"
+    static_map_params = {"ll": f"{longitude},{latitude}", "l": map_type, "z": 14}
+    response = requests.get(static_map_url, params=static_map_params)
+    Image.open(BytesIO(response.content)).show()
+    print("Для показа следующего слайда нажмите Enter")
+    input()
